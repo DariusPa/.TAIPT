a9852ac0d112f475781c459c51065bc0dd29e11b
AzuolasMisevicius <azuolasmisevicius@gmail.com>
2018-10-13 16:33:10 +0300

    update

---
+++ VirtualLibrarian/Library.Tests/UnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using VirtualLibrarian;
+
+namespace Library.Tests
+{
+    [TestFixture]
+    class Unit_Tests
+    {
+        [TestCase("labasgmail.com")]
+        [TestCase("laba..s@gmail.com")]
+        [TestCase("labas@gmail.")]
+        [TestCase("labas@.com")]
+        public void validatesEmail(string email)
+        {
+            var sut = new RegexUtilities();
+            Assert.AreEqual(false, sut.IsValidEmail(email));
+        }
+    }
+}
